63a1d41bb0d72e213eb4a067c7840d01175e4458
sunflour322 <myasnoypelmesh@gmail.com>
2025-02-21 03:27:27 +0300

    start work

---
+++ src/main/java/com/meetcake/meetcake_back/MeetcakeBackApplication.java
@@ -0,0 +1,13 @@
+package com.meetcake.meetcake_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetcakeBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetcakeBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meetcake/meetcake_back/MeetcakeBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meetcake.meetcake_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeetcakeBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
